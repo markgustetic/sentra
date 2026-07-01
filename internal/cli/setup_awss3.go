4d1cb752278e7a5c7934d5b8cfa11ccabb2ed47c
Mark Gustetic <mark.gustetic@gmail.com>
2026-07-01 14:44:04 -0400

    Split cli/setup_awss3.go: move low-level S3 helpers into setup_awss3_ops.go

--- internal/cli/setup_awss3.go
+++ internal/cli/setup_awss3.go
@@ -2,22 +2,14 @@ package cli
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"time"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	awsconfig "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
-	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/aws/aws-sdk-go-v2/service/sts"
-	"github.com/aws/smithy-go"
 
 	"github.com/markgustetic/sentra/internal/blobstore"
 	"github.com/markgustetic/sentra/internal/config"
 )
-
-const bucketExistsWaitTimeout = 2 * time.Minute
 
 // AWSInspectReport summarizes read-only AWS bucket diagnostics.
 type AWSInspectReport struct {
@@ -127,161 +119,4 @@ func DefaultAWSPrepare(ctx context.Context, cfg *config.Config, opts AWSPrepareO
 		report.DefaultEncryptionEnabled = true
 	}
 	return report, nil
-}
-
-func loadSetupAWSConfig(ctx context.Context, cfg *config.Config) (aws.Config, error) {
-	loadOpts := []func(*awsconfig.LoadOptions) error{}
-	if cfg != nil {
-		if cfg.Repo.S3.Region != "" {
-			loadOpts = append(loadOpts, awsconfig.WithRegion(cfg.Repo.S3.Region))
-		}
-		if cfg.Repo.S3.Profile != "" {
-			loadOpts = append(loadOpts, awsconfig.WithSharedConfigProfile(cfg.Repo.S3.Profile))
-		}
-	}
-	awsCfg, err := awsconfig.LoadDefaultConfig(ctx, loadOpts...)
-	if err != nil {
-		return aws.Config{}, fmt.Errorf("load AWS config: %w", err)
-	}
-	return awsCfg, nil
-}
-
-func headBucket(ctx context.Context, client *s3.Client, bucket string) error {
-	_, err := client.HeadBucket(ctx, &s3.HeadBucketInput{Bucket: aws.String(bucket)})
-	if err != nil {
-		return fmt.Errorf("head bucket %q (requires s3:ListBucket on %s): %w", bucket, s3BucketARN(bucket), err)
-	}
-	return nil
-}
-
-func createBucket(ctx context.Context, client *s3.Client, bucket, region string) (bool, error) {
-	input := &s3.CreateBucketInput{Bucket: aws.String(bucket)}
-	if region != "" && region != "us-east-1" {
-		input.CreateBucketConfiguration = &types.CreateBucketConfiguration{
-			LocationConstraint: types.BucketLocationConstraint(region),
-		}
-	}
-	_, err := client.CreateBucket(ctx, input)
-	if err == nil {
-		return true, nil
-	}
-	if isBucketAlreadyOwned(err) {
-		return false, nil
-	}
-	return false, fmt.Errorf("create bucket %q (requires s3:CreateBucket on %s): %w", bucket, s3BucketARN(bucket), err)
-}
-
-func waitForBucketExists(ctx context.Context, client *s3.Client, bucket string) error {
-	waiter := s3.NewBucketExistsWaiter(client)
-	err := waiter.Wait(ctx, &s3.HeadBucketInput{Bucket: aws.String(bucket)}, bucketExistsWaitTimeout)
-	if err != nil {
-		return fmt.Errorf("wait for bucket %q to exist (requires s3:ListBucket on %s): %w", bucket, s3BucketARN(bucket), err)
-	}
-	return nil
-}
-
-func blockBucketPublicAccess(ctx context.Context, client *s3.Client, bucket string) error {
-	_, err := client.PutPublicAccessBlock(ctx, &s3.PutPublicAccessBlockInput{
-		Bucket: aws.String(bucket),
-		PublicAccessBlockConfiguration: &types.PublicAccessBlockConfiguration{
-			BlockPublicAcls:       aws.Bool(true),
-			IgnorePublicAcls:      aws.Bool(true),
-			BlockPublicPolicy:     aws.Bool(true),
-			RestrictPublicBuckets: aws.Bool(true),
-		},
-	})
-	if err != nil {
-		return fmt.Errorf("block public access for bucket %q (requires s3:PutBucketPublicAccessBlock on %s): %w", bucket, s3BucketARN(bucket), err)
-	}
-	return nil
-}
-
-func enableBucketDefaultEncryption(ctx context.Context, client *s3.Client, bucket string) error {
-	_, err := client.PutBucketEncryption(ctx, &s3.PutBucketEncryptionInput{
-		Bucket: aws.String(bucket),
-		ServerSideEncryptionConfiguration: &types.ServerSideEncryptionConfiguration{
-			Rules: []types.ServerSideEncryptionRule{
-				{
-					ApplyServerSideEncryptionByDefault: &types.ServerSideEncryptionByDefault{
-						SSEAlgorithm: types.ServerSideEncryptionAes256,
-					},
-				},
-			},
-		},
-	})
-	if err != nil {
-		return fmt.Errorf("enable default encryption for bucket %q (requires s3:PutBucketEncryption on %s): %w", bucket, s3BucketARN(bucket), err)
-	}
-	return nil
-}
-
-func getBucketPublicAccessBlock(ctx context.Context, client *s3.Client, bucket string) (bool, bool, error) {
-	out, err := client.GetPublicAccessBlock(ctx, &s3.GetPublicAccessBlockInput{Bucket: aws.String(bucket)})
-	if err != nil {
-		if isAWSAPIErrCode(err, "NoSuchPublicAccessBlockConfiguration", "NoSuchPublicAccessBlock") {
-			return true, false, nil
-		}
-		return false, false, fmt.Errorf("inspect public access block for bucket %q (requires s3:GetBucketPublicAccessBlock on %s): %w", bucket, s3BucketARN(bucket), err)
-	}
-	cfg := out.PublicAccessBlockConfiguration
-	blocked := cfg != nil &&
-		aws.ToBool(cfg.BlockPublicAcls) &&
-		aws.ToBool(cfg.IgnorePublicAcls) &&
-		aws.ToBool(cfg.BlockPublicPolicy) &&
-		aws.ToBool(cfg.RestrictPublicBuckets)
-	return true, blocked, nil
-}
-
-func getBucketDefaultEncryption(ctx context.Context, client *s3.Client, bucket string) (bool, bool, error) {
-	out, err := client.GetBucketEncryption(ctx, &s3.GetBucketEncryptionInput{Bucket: aws.String(bucket)})
-	if err != nil {
-		if isAWSAPIErrCode(err, "ServerSideEncryptionConfigurationNotFoundError") {
-			return true, false, nil
-		}
-		return false, false, fmt.Errorf("inspect default encryption for bucket %q (requires s3:GetBucketEncryption on %s): %w", bucket, s3BucketARN(bucket), err)
-	}
-	cfg := out.ServerSideEncryptionConfiguration
-	return true, cfg != nil && len(cfg.Rules) > 0, nil
-}
-
-func isS3BucketMissing(err error) bool {
-	var apiErr smithy.APIError
-	if !errors.As(err, &apiErr) {
-		return false
-	}
-	switch apiErr.ErrorCode() {
-	case "NotFound", "NoSuchBucket", "404":
-		return true
-	default:
-		return false
-	}
-}
-
-func isBucketAlreadyOwned(err error) bool {
-	var apiErr smithy.APIError
-	return errors.As(err, &apiErr) && apiErr.ErrorCode() == "BucketAlreadyOwnedByYou"
-}
-
-func isAWSAPIErrCode(err error, codes ...string) bool {
-	var apiErr smithy.APIError
-	if !errors.As(err, &apiErr) {
-		return false
-	}
-	for _, code := range codes {
-		if apiErr.ErrorCode() == code {
-			return true
-		}
-	}
-	return false
-}
-
-func s3BucketARN(bucket string) string {
-	return "arn:aws:s3:::" + bucket
-}
-
-func s3ObjectARN(bucket string, prefix string) string {
-	if prefix == "" {
-		return s3BucketARN(bucket) + "/*"
-	}
-	return s3BucketARN(bucket) + "/" + prefix + "*"
 }
